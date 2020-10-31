db95a8896e53a33168f0fa8a985250abb27e318a
Michael Bürstner <michael@buerstner.net>
2020-10-31 11:15:08 +0100

    basic functions, desktop version

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const registerEvents = function() {
-    let headlines = document.querySelectorAll(".faq-item>h2");
-    console.log(headlines);
-    for (let headline of headlines) {
+    let faqItems = document.querySelectorAll(".faq-item");
+    for (let faqItem of faqItems) {
+        let headline = faqItem.firstElementChild;
         headline.addEventListener("mouseover", e => {
            e.target.style.color = "hsl(14, 88%, 65%)";
            e.target.style.cursor = "pointer";
@@ -12,7 +12,19 @@ const registerEvents = function() {
             e.target.style.color = "hsl(238, 29%, 16%)";
         });
         headline.addEventListener("click", e => {
-            console.log(e);
+            let faqTextParagraph = faqItem.querySelector("p");
+            if (faqTextParagraph.className === "hidden") {
+                let faqTexts = document.querySelectorAll(".faq-item > p");
+                faqTexts.forEach(f => {
+                    f.setAttribute("class", "hidden");
+                });
+                faqTextParagraph.className="";
+                headline.lastElementChild.className="arrow-up";
+            } else {
+                faqTextParagraph.className="hidden";
+                headline.lastElementChild.className="";
+            }
+ 
         });
     }
 }
